5427c9c4871e7b1a0cf77e5593c46cc5aff23cb3
austin samsel <austinsamsel@gmail.com>
2017-02-07 22:17:21 -0500

    able to deploy to now

--- src/config/firebase/index.js
+++ src/config/firebase/index.js
@@ -1,15 +1,19 @@
 const admin = require('firebase-admin')
 
-const fb_key = process.env.FIREBASE_KEY
-const fb_suffix = 'END PRIVATE KEY-----\n'
-const fb_concat = fb_key + fb_suffix 
-const firebase_key = fb_concat.replace(/\\n/g, '\n')
+const pre = '-----BEGIN@PRIVATE@KEY-----';
+const suf = '-----END@PRIVATE@KEY-----_n';
+
+const fb_concat = pre + process.env.FIREBASE_KEY + suf;
+
+const fb_key = fb_concat.replace(/@/g, ' ');
+const fb_key2 = fb_key.replace(/_/g, '\\')
+const fb_key3 = fb_key2.replace(/\\n/g, '\n')
 
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_ID,
     clientEmail: process.env.FIREBASE_EMAIL,
-    privateKey: firebase_key
+    privateKey: fb_key3
   }),
   databaseURL: process.env.FIREBASE_URL
 });
