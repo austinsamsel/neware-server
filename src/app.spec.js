87dd529805cb10ba168ed862c0e6039dfafe2497
austin samsel <austinsamsel@gmail.com>
2017-03-14 20:57:01 -0400

    linting good enough for now

--- src/app.spec.js
+++ src/app.spec.js
@@ -7,17 +7,17 @@ require('./config/firebase/test_server') // firebase config
 const db_test_ref = admin.database().ref('___test_read_write')
 
 const sample_post = (new_time) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     db_test_ref.set({
       time: new_time,
-    }).then(()=> {
+    }).then(() => {
       resolve()
     })
   })
 }
 
 const read_sample_post = (new_time) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     db_test_ref.once('value').then(function(snapshot) {
       const time = snapshot.val().time;
       resolve(time)
@@ -27,14 +27,12 @@ const read_sample_post = (new_time) => {
 
 test('write and read firebase', (t) => {
   const new_time = Date.now();
-  sample_post(new_time)
-  .then(() => {
+  sample_post(new_time).then(() => {
     read_sample_post().then((res) => {
       console.log(res)
       t.equal(res, new_time, 'found post with same time')
       t.end();
-    })
-    .then(() => {
+    }).then(() => {
       util.delete_sample_post(db_test_ref);
     })
   })
