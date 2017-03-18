f7432b29f49f521db4fe5d3352a5b53240b4223e
austin samsel <austinsamsel@gmail.com>
2017-03-18 10:01:46 -0400

    run firebase cleaner on visit

--- src/app.js
+++ src/app.js
@@ -3,29 +3,23 @@ import express from 'express';
 const app = express();
 require('dotenv').config()   // helps parse config
 require('./config/firebase/live_server') // firebase config
-// firebase service worker: clean old posts
+// firebase service worker to clean old posts
 const fb_clean = require('./modules/fb_clean');
 
 app.set('port', (process.env.PORT || 5000));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (request, response) {
   const greeting = process.env.GREETING;
   response.writeHead(200)
   response.end(greeting)
+  // run firebase cleaner
+  fb_clean.clean('notes'); 
 });
 
-// To run on server startup
-const startup = () => {
-  //start firebase clean up.
-  fb_clean.clean('notes');   
-}
-// listen for requests :)
-// we'll ping every 24 hours.
-// which will kick off all the firebase stuff.
 app.listen(app.get('port'), function() {
   console.log('Your app is listening on port ' + app.get('port'));
-  startup();
+  // run firebase cleaner
+  fb_clean.clean('notes');
 });
 
 
--- src/modules/fb_clean.js
+++ src/modules/fb_clean.js
@@ -3,8 +3,8 @@ import admin from 'firebase-admin'
 const db = admin.database();
 const one_day = 1000 * 60 * 60 * 24;
 //const five_min  = 1000 * 60 * 5 // for testing
-//const one_min  = 1000 * 60 * 1  // for testing
-const cutoff = Date.now() - (one_day);
+const one_min  = 1000 * 60 * 1  // for testing
+const cutoff = Date.now() - (one_min);
 
 const clean = (notes) => {
   // delete old posts
